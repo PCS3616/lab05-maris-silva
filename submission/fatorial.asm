004d13a7a6751a4351946a522341b1cb7a80a0f9
Mariana Silva <mariana.silva.a46@hotmail.com>
2025-02-11 16:18:21 -0300

    revisao, melhora de codigos e adicao de comentarios

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,24 +1,24 @@
-	SC SUB_ini
+	SC SUB_ini	;chama a subrotina
 	HM /000
-	
-	@ /0010
+
+	@ /0010		;comeca subrotina em 0x0012, 0x0010 fica vago pra retorno
 SUB_ini K /000
-	LD /100
-	MM /104
-	JZ SUB_fim
-FOR_ini	LD RES
+	LD N		;faz load do N passado
+	MM I		;salva na variavel de iteracao i
+	JZ SUB_fim	;checa se N != 0, caso qnd o resultado ja esta armazenado por padrao
+FOR_ini	LD RES		;senao, multiplica por mais um termo
 	ML I
 	MM RES
-	LD I
+	LD I		;atualiza i, decrementando em 1
 	SB UM
 	MM I
-	JZ SUB_fim
-	JP FOR_ini
+	JZ SUB_fim	;se i==0, sai do loop
+	JP FOR_ini	;senao, faz outra iteracao
 SUB_fim RS SUB_ini
-	
 
+;variaveis e valores auxiliares
 	@ /0100
 N 	K =0
-RES 	K =1
+RES 	K =1		;eh inicializado com 1, para caso N == 0
 I 	K =0
-UM	K =1
+UM	K =1		;cte
--- submission/io.asm
+++ submission/io.asm
@@ -1,31 +1,37 @@
+;le e armazena X
 	GD /000
 	SB ASCII_D
 	MM X
+;pula espacos
 	GD /000
+;le e armazena Y
 	GD /000
 	SB ASCII_D
 	MM Y
+;calcula e armazena X+Y
 	LD X
 	AD Y
 	MM RES
+;checa se o segundo digito do resultado eh maior que 9 por meio de shifts
 	ML SHIFT
-	DV SHIFT
 	SB NOVE
 	JN PRINT
+;se for, faz o vai-um
 	LD RES
 	SB CTE_A
 	AD CTE_256
+;senao, soma 0x3030 e printa resultado. tambem chega aqui apos o tratamento
 PRINT	AD ASCII_D
 	PD /100
 	HM /000
 
-
+;variaveis e valores auxiliares
 	@ /0100
 ASCII_D	K /3030
 SHIFT	K /0100
 X	K =0
 Y	K =0
 RES	K =0
-NOVE	K /0009
+NOVE	K /0900
 CTE_A	K /A
 CTE_256	K /0100
